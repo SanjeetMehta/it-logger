a75bfdbd5788bcab79593c0777bf4c508ff08ed8
Sanjeetkumar Mehta <sanjeetkmehta@gmail.com>
2020-08-01 13:42:17 +0530

    Implemented get log action

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from "react";
-import LogItem from "./LogItem";
-import Preloader from "../layout/Preloader";
-import {connect} from "react-redux";
 import PropTypes from "prop-types";
+import React, {useEffect} from "react";
+import {connect} from "react-redux";
 import {getLogs} from "../../actions/logActions";
+import Preloader from "../layout/Preloader";
+import LogItem from "./LogItem";
 
 const Logs = ({log: {logs, loading}, getLogs}) => {
     useEffect(() => {
@@ -30,7 +30,8 @@ const Logs = ({log: {logs, loading}, getLogs}) => {
 };
 
 Logs.propTypes = {
-    log: PropTypes.object.isRequired
+    log: PropTypes.object.isRequired,
+    getLogs: PropTypes.func.isRequired
 };
 const mapStateToProps = state => ({
     log: state.log
