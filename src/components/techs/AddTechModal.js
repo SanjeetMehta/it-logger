7533ef9bf38f35310192411c1b708965a1bc6f9b
Sanjeetkumar Mehta <sanjeetkmehta@gmail.com>
2020-08-01 22:10:42 +0530

    Added functionality for deleting techs

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, {useState} from "react";
+import {connect} from "react-redux";
 import M from "materialize-css/dist/js/materialize.min.js";
+import PropTypes from "prop-types";
+import {addTech} from "../../actions/techActions";
 
-const AddTechModal = props => {
+const AddTechModal = ({addTech}) => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
 
@@ -9,7 +12,11 @@ const AddTechModal = props => {
         if (!firstName || !lastName) {
             M.toast({html: "Please enter a the first and last name."});
         } else {
-            console.log(firstName, lastName);
+            addTech({
+                firstName,
+                lastName
+            });
+            M.toast({html: `${firstName} ${lastName} was added as tech`});
             setFirstName("");
             setLastName("");
         }
@@ -59,4 +66,8 @@ const AddTechModal = props => {
     );
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+    addTech: PropTypes.func.isRequired
+};
+
+export default connect(null, {addTech})(AddTechModal);
